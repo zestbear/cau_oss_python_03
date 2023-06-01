a16a1232e2f44e5b5cbc8b06a7d3d75ddce96628
HYUN <zestbear90@gmail.com>
2023-06-01 12:44:04 +0900

    implement hw5

--- figure.py
+++ figure.py
@@ -1,21 +1,27 @@
 import math
 
 class line:
-    __length = 0
-    def __init__(self, length):
-        self.__length = length
+    __width  = 0
+    __height = 0
+    def __init__(self, width, height):
+        self.__width  = width
+        self.__height = height
 
-    def __set_length(self, length):
-        self.__length = length
+    def __set_length(self, width, height):
+        self.__width  = width
+        self.__height = height
 
     def get_length(self):
-        return self.__length
+        return self.__width, self.__height
     
-def area_square(length):
-    return length * length
+def area_rectangle(width, height):
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height
 
-def area_circle(length):
-    return length * length * math.pi
+def area_ellipse(width, height):
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height * math.pi
 
-def area_regular_triangle(length):
-    return length * length * math.sqrt(3) / 4
+def area_right_triangle(width, height):
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height / 2
--- figure_main.py
+++ figure_main.py
@@ -1,13 +1,41 @@
+# %%
+
+"""
+area_rectangle, area_right_triangle, area_ellipse 함수들에서
+예외 발생 시 0이하의 값은 입력할 수 없습니다를 출력하도록 변경!
+"""
+
 import figure
-myline = figure.line(10)
 
-square = figure.area_square(myline.get_length())
-print(square)
+myline = figure.line(10, 20)
+width, height = myline.get_length()
+# implement exception handler
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("please input positive number for width and height")
+rectangle = figure.area_rectangle(width, height)
+print(rectangle)
 
-myline.__set_length(20)
-regular_triangle = figure.area_regular_triangle(myline.get_length())
-print(regular_triangle)
+myline.set_length(20, 30)
+width, height = myline.get_length()
+# implement exception handler
+try:
+    triangle = figure.area_right_triangle(width, height)
+    print(triangle)
+except ValueError:
+    print("please input positive number for width and height")
+triangle = figure.area_right_triangle(width, height)
+print(triangle)
 
-myline.__set_length(30)
-circle = figure.area_circle(myline.get_length())
-print(circle)
+myline.set_length(30, 40)
+width, height = myline.get_length()
+# implement exception handler
+try:
+    ellipse = figure.area_ellipse(width, height)
+    print(ellipse)
+except ValueError:
+    print("please input positive number for width and height")
+ellipse = figure.area_ellipse(width, height)
+print(ellipse)
